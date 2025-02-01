7d0fc16cfdb63b3d3b5c9fabe183e11b27d385f8
Alina <alina84770@.com>
2025-02-01 09:03:32 +0500

    update code

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './BlogCard.css';
 import { Link } from 'react-router-dom';
+import { FaUser } from "react-icons/fa";
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-const BlogCard = ({ id, image, title, author, publishDate }) => {
+const BlogCard = ({ id, image, title, author, publishData }) => {
   {/* animation on scroll */ }
   useEffect(() => {
     AOS.init({ duration: 1000 });
@@ -18,8 +19,8 @@ const BlogCard = ({ id, image, title, author, publishDate }) => {
           <img src={image} className='w-100 p-2 rounded' loading='lazy' />
           <div className='px-2'>
             <h6>{title}</h6>
-            <p>{author}</p>
-            <p>published: {publishDate}</p>
+            <p className='d-flex align-items-center gap-3'><FaUser /> {author}</p>
+            <p>published: {publishData}</p>
           </div>
         </div>
       </Link>
